d73d89f5fadfd6a55e958e8da4eed920ae6aeb3a
Brent Kerr <css@codecamel.com>
2010-05-04 19:13:38 +0000

    moved project list to separate file
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/web@2353 4407c894-4637-0410-b4f5-ada5f102cad1

---
+++ htdocs/projects.php
@@ -0,0 +1,38 @@
+<li><a href="http://gcc.gnu.org/">GCC: The GNU Compiler Collection</a></li>
+<li><a href="http://www.gnu.org/software/binutils/">GNU Binutils</a></li>
+<li><a href="http://www.gnu.org/software/gdb/">GDB: The GNU Project Debugger</a></li>
+<li><a href="http://fedoraproject.org/wiki/MinGW">Fedora cross-compiler</a></li>
+
+<li><a href="http://uhexen2.sf.net/">Hexen II: Hammer of Thyrion</a></li>
+<li><a href="http://ffmpeg.mplayerhq.hu/">FFmpeg</a></li>
+<li><a href="http://www.opensc-project.org/">OpenSC</a></li>
+<li><a href="http://wiki.winehq.org/Wine64">Wine</a></li>
+<li><a href="http://mamedev.org/">MAME</a> (Yes, the arcade emulator!)</li>
+
+<li><a href="http://www.reactos.org/en/index.html">ReactOS</a></li>
+<li><a href="http://www.videolan.org/vlc/">VideoLAN VLC</a></li>
+<li><a href="http://sourceware.org/pthreads-win32/">pthreads</a></li>
+<li><a href="http://www.openssl.org/">OpenSSL</a></li>
+<li><a href="http://www.wxwidgets.org/">wxWidgets</a></li>
+<li><a href="http://www.codeblocks.org/">Code::Blocks</a></li>
+
+<li><a href="http://www.fltk.org/">FLTK</a></li>
+<li><a href="http://sbcarchiver.cjb.net/">SBC Archiver</a></li>
+<li><a href="http://www.eligis.com/">OpenLisp</a></li>
+<li><a href="http://www.gtk.org/download-windows-64bit.html">GTK+</a></li>
+<li><a href="http://gimp-win.sourceforge.net/stable.html">GIMP</a></li>
+<li><a href="http://www.mpg123.de/">mpg123</a></li>
+
+<li><a href="http://factorcode.org/">Factor</a></li>
+<li><a href="http://jpen.sf.net/">JPen</a></li>
+<li><a href="http://www.iauxsoft.com/">iAuxSoft</a></li>
+<li><a href="http://remood.sf.net/">ReMooD</a></li>
+<li><a href="http://emergedesktop.org">Emerge Desktop</a></li>
+<li><a href="http://www.mega-nerd.com/libsndfile/">libsndfile</a></li>
+
+<li><a href="http://www.wxperl.co.uk/building/msw.html">wxPerl PPMs</a></li>
+<li><a href="http://www.zlib.net/">zlib</a></li>
+<li><a href="http://www.r-project.org/">The R Project for Statistical Computing</a></li>
+<li><a href="http://www.perl.org/">Perl</a> (5.12.0 and later)</li>
+<li><a href="http://strawberryperl.com/">Strawberry Perl</a> (contains bundled mingw-w64 gcc toolchain)</li>
+<li><a href="mailto:mingw-w64-public@lists.sourceforge.net">Add your project!</a></li>
