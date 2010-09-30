a268e5d8ea80d9c3e62b2b312633c6554874edae
Jonathan Yong <10walls@gmail.com>
2010-09-30 10:07:26 +0000

    Add GnuTLS to projects list.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/web@3680 4407c894-4637-0410-b4f5-ada5f102cad1

--- htdocs/projects.php
+++ htdocs/projects.php
@@ -37,4 +37,5 @@
 <li><a href="http://strawberryperl.com/">Strawberry Perl</a> (contains bundled mingw-w64 gcc toolchain)</li>
 <li><a href="http://quakespasm.sourceforge.net/">QuakeSpasm</a></li>
 <li><a href="http://www.gnu.org/software/gsasl/">GNU SASL</a></li>
+<li><a href="http://www.gnu.org/software/gnutls/">GnuTLS</a></li>
 <li><a href="mailto:mingw-w64-public@lists.sourceforge.net">Add your project!</a></li>
