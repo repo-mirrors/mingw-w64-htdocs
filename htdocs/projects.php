4fc2bf6764b17bccbc9012889e6790050a561e40
Jonathan Yong <10walls@gmail.com>
2011-05-23 10:33:45 +0000

    Add ManKai Common Lisp to project list.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/web@4177 4407c894-4637-0410-b4f5-ada5f102cad1

--- htdocs/projects.php
+++ htdocs/projects.php
@@ -48,6 +48,7 @@ $projects = array(
 	'<a href="http://www.yafaray.org/">YafaRay</a>',
         '<a href="http://libav.org/">libav</a>',
         '<a href="http://www.mpir.org/">MPIR</a>',
+        '<a href="http://common-lisp.net/project/mkcl/">ManKai Common Lisp</a>',
 	'<a href="http://mctrl.sourceforge.net/">mCtrl</a>');
 
 
