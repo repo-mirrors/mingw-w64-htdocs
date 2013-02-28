d752cb3805cecabd37dc001141e9d98d8e97b0cf
NightStrike <nightstrike@gmail.com>
2013-02-28 17:09:41 +0000

    Add Npackd to the list of projects using mingw-w64
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/web@5624 4407c894-4637-0410-b4f5-ada5f102cad1

--- htdocs/projects.php
+++ htdocs/projects.php
@@ -68,7 +68,8 @@ $projects = array(
         '<a href="http://botan.randombit.net/">Botan</a>',
         '<a href="http://www.woodem.eu/">Woo</a>',
         '<a href="http://qt-project.org/">Qt 4.8.3</a>',
-        '<a href="http://www.ecere.com/">Ecere SDK - eC compiler, GUI toolkit, and IDE</a>');
+        '<a href="http://www.ecere.com/">Ecere SDK - eC compiler, GUI toolkit, and IDE</a>'
+        '<a href="http://code.google.com/p/windows-package-manager/">Npackd</a>');
 
 	shuffle($projects);
 	foreach($projects as $project) echo '<li>'.$project.'</li>';
