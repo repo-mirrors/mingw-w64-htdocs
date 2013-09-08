6d996cd5425391349d740b6a8066a413a7f8d205
Adrien Nader <adrien@notk.org>
2013-09-08 09:39:35 +0000

    web: change "contribute" to "donate" and don't say all devs are unpaid.
    The link is to the donation page so "donate" is a better match.
    
    Moreover, not all devs are unpaid for mingw-w64 so only mention that
    "most of them" are not paid.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/web@6239 4407c894-4637-0410-b4f5-ada5f102cad1

--- htdocs/inc/right.php
+++ htdocs/inc/right.php
@@ -12,5 +12,5 @@
 ?>
       <a href="http://sourceforge.net/projects/mingw-w64/support" class="help">Help!</a>
       <a href="irc://irc.oftc.net/#mingw-w64" class="chat">Join IRC Channel #mingw-w64 on oftc.net</a>
-      <a href="https://sourceforge.net/donate/index.php?group_id=202880" class="donate"><span>Contribute</span>The developers of MinGW-w64 are un-paid, and work on it in their spare time.</a>
+      <a href="https://sourceforge.net/donate/index.php?group_id=202880" class="donate"><span>Donate</span>Most MinGW-w64 developers are un-paid and work on it in their spare time.</a>
     </div>
