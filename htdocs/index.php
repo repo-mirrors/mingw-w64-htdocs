2004cf568c595de67a7e4c04ab36f7ace5e7c830
Adrien Nader <adrien@notk.org>
2013-10-13 20:14:17 +0000

    web: make it clear that GCC [4.5.1; 4.7.x] is only supported for CRTv2.
    Because of the formatting, it was possible to believe that GCC versions
    from 4.5.1 to 4.7.x were supported for CRTv3 too.
    
    Instead of mentionning CRTv2 and these GCC versions in two separate <p>
    blocks, put them in the same so that it is clear this relates to v2.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/web@6341 4407c894-4637-0410-b4f5-ada5f102cad1

--- htdocs/index.php
+++ htdocs/index.php
@@ -37,10 +37,8 @@
         </ul>
       </p>
       <p>
-        <em>Version 2.0</em> is the previous release; it greatly expanded the support for Windows Vista/7 APIs.
-      </p>
-      <p>
-        Use the latest point version of GCC 4.7 or 4.8. Versions down to 4.5.1 are supported but each new version brings many improvements.
+        <em>Version 2.0</em> is the previous release; it greatly expanded the support for Windows Vista/7 APIs.<br>
+        Use the latest GCC version; versions down to 4.5.1 are supported but each new version brings many improvements.
       </p>
       <p>
         The <a href="http://sourceforge.net/projects/mingw-w64">project page</a> has the full list of <a href="http://sourceforge.net/projects/mingw-w64/files/">releases</a> and more <a href="http://sourceforge.net/apps/trac/mingw-w64/">details</a>.
