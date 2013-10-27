f80d7a73c5aa903067b8633437008af06af2a877
Adrien Nader <adrien@notk.org>
2013-10-27 08:56:50 +0000

    web: mention OpenSuse builds on the download page.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/web@6361 4407c894-4637-0410-b4f5-ada5f102cad1

--- htdocs/downloads/others.php
+++ htdocs/downloads/others.php
@@ -1,7 +1,7 @@
 <h2 id="others">Other Downloads</h2>
 <div class="toolchain">
   <p>
-    There are more toolchains available than the ones mentioned here. Most notable one is Ruben's (with toolchains targeting <a href="http://sf.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/rubenvb/">Win32</a> and <a href="http://sf.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/rubenvb/">Win64</a>, some with Dwarf2 Exception Handling along with clang binaries).
+    There are more toolchains available than the ones mentioned here. Most notable one is Ruben's (with toolchains targeting <a href="http://sf.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/rubenvb/">Win32</a> and <a href="http://sf.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/rubenvb/">Win64</a> along with the ones that available in OpenSuse and some with Dwarf2 Exception Handling along with clang binaries).
   </p>
   <p>
     There are even more downloads (other toolchains, older versions, additional binaries, ...) available from the <a href="http://sf.net/projects/mingw-w64/files/">file hosting on SourceForge</a>.
