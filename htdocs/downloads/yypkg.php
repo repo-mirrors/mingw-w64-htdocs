b342bf8253dee58206fec343f41a7e9a07b21ede
Adrien Nader <adrien@notk.org>
2013-12-11 13:04:56 +0000

    downloads: the win-builds project is now around version 1.3.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/web@6410 4407c894-4637-0410-b4f5-ada5f102cad1

--- htdocs/downloads/yypkg.php
+++ htdocs/downloads/yypkg.php
@@ -14,7 +14,7 @@
     <tr>
       <td>C, C++</td>
       <td>Yypkg</td>
-      <td>SJLJ</td>
+      <td>SJLJ for i686; SEH for x64</td>
       <td>POSIX</td>
     </tr>
   </table>
@@ -30,9 +30,9 @@
     <tr>
       <td>Linux 64</td>
       <td>i686, x86_64</td>
-      <td rowspan="3">4.7.2</td>
-      <td rowspan="3">2.0.7</td>
-      <td rowspan="4"><a href="http://win-builds.org">win-builds.org</a></td>
+      <td rowspan="3">4.8.2</td>
+      <td rowspan="3">3.0.1</td>
+      <td rowspan="3"><a href="http://win-builds.org">win-builds.org</a></td>
     </tr>
     <tr>
       <td>Windows 32</td>
@@ -48,22 +48,16 @@
       <!-- cell is defined by a rowspan in the previous <tr> -->
       <!-- cell is defined by a rowspan in the previous <tr> -->
     </tr>
-    <tr>
-      <td>Linux 64 (alpha)</td>
-      <td>i686, x86_64</td>
-      <td>4.8.1</td>
-      <td>3.0.0</td>
-      <!-- cell is defined by a rowspan in the previous <tr> -->
-    </tr>
   </table>
   <p>
     <strong>Additional Software</strong>: atk, c-ares, ca-certificates,
-    cairo, curl, ecore, edje, eet, eina, elementary, embryo, evas, evil,
+    cairo, curl, dbus, ecore, edje, eet, eina, elementary, embryo, evas, evil,
     expat, flexdll, fontconfig, freetype, gdk-pixbuf2, gettext, giflib,
     glib-networking, glib2, gmp, gnutls, gperf, gtk+2, icu4c, libarchive,
     libffi, libjpeg, libmpc, libogg, libpng, libsoup, libtheora, libtiff,
-    libvorbis, libxml2, libxslt, lua, mpfr, nettle, ocaml, ocaml-findlib,
-    openssl, pango, pixman, pkg-config, sqlite, wget, win-iconv, xz, zlib.
+    libvorbis, libxml2, lua, mpfr, nettle, ocaml, ocaml-findlib,
+    openssl, pango, pixman, pkg-config, sqlite, wget, win-iconv, winpthreads, 
+    x264, xz, zlib.
     </p>
 </div>
 
