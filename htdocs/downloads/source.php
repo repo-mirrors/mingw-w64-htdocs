2e32c48a5e59ce220dd2ec800cc23e6eb656778e
Adrien Nader <adrien@notk.org>
2014-02-25 22:17:52 +0000

    web: update the paragrahs for sources to version 3.1.0.
    (this has been on the website for months but without being commited)
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/mingw-w64/code/web@6505 4407c894-4637-0410-b4f5-ada5f102cad1

--- htdocs/downloads/source.php
+++ htdocs/downloads/source.php
@@ -1,10 +1,10 @@
 <h2 id="source">Source</h2>
 <div class="toolchain">
   <p id="source-crt">
-    Tarballs for the mingw-w64 sources are hosted on <a href="http://sourceforge.net/projects/mingw-w64/files/mingw-w64/mingw-w64-release/">SourceForge</a>. The latest version is <a href="http://sourceforge.net/projects/mingw-w64/files/mingw-w64/mingw-w64-release/mingw-w64-v3.0.0.tar.bz2/download">3.0.0</a>.
+    Tarballs for the mingw-w64 sources are hosted on <a href="http://sourceforge.net/projects/mingw-w64/files/mingw-w64/mingw-w64-release/">SourceForge</a>. The latest version is <a href="http://sourceforge.net/projects/mingw-w64/files/mingw-w64/mingw-w64-release/mingw-w64-v3.1.0.tar.bz2/download">3.1.0</a>.
   </p>
   <p id="source-winpthreads">
-    Winpthreads is available in the main tarball as of 3.0.0. For previous versions of the CRT, you need to <a href="http://sourceforge.net/p/mingw-w64/code/6184/tree/trunk/mingw-w64-libraries/winpthreads/">checkout the SVN repository for mingw-w64-libraries</a>.
+    Winpthreads is available in the main tarball as of 3.1.0. For previous versions of the CRT, you need to <a href="http://sourceforge.net/p/mingw-w64/code/6184/tree/trunk/mingw-w64-libraries/winpthreads/">checkout the SVN repository for mingw-w64-libraries</a>.
   </p>
   <p>
     The wiki has instructions for building <a href="http://sourceforge.net/apps/trac/mingw-w64/wiki/Native%20Win64%20compiler">native</a> and <a href="http://sourceforge.net/apps/trac/mingw-w64/wiki/Cross%20Win32%20and%20Win64%20compiler">cross</a> toolchains.
