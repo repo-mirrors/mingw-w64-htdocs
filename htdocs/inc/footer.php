8c48f686dd16618645560de12c33ab7abb44cbde
Adrien Nader <adrien@notk.org>
2014-06-16 22:29:19 +0200

    footer: remove href to the codecamel website which is now dead.

--- htdocs/inc/footer.php
+++ htdocs/inc/footer.php
@@ -9,7 +9,7 @@
         <a href="http://validator.w3.org/check?uri=referer">Valid HTML</a> | 
         <a href="irc://irc.oftc.net/#mingw-w64">Discuss</a></p>
       <p>
-        <a href="http://codecamel.com">Website donated by Codecamel</a>
+        Website donated by Codecamel
 <?php
       if (isset($page_authors)) {
         echo '| Page by ' . $page_authors;
