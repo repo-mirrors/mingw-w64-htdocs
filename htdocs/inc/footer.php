62b0091d2d1242c59013f73e5b11e5b0af04f5e8
Adrien Nader <adrien@notk.org>
2014-06-16 22:31:41 +0200

    footer: update copyright to 2014.

--- htdocs/inc/footer.php
+++ htdocs/inc/footer.php
@@ -4,8 +4,9 @@
     <!-- FOOTER -->
     <div id="footer">
       <p>
-        Copyright &copy; 2010
-        <a href="http://validator.w3.org/check?uri=referer">Valid HTML</a> | 
+        Copyright &copy; 2014 |
+        <a href="http://validator.w3.org/check?uri=referer">Valid HTML</a>
+      </p>
       <p>
         Website donated by Codecamel
 <?php
