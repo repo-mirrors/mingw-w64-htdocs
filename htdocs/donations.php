5fbb58780c8c99111acc95476e85564b1e372714
Adrien Nader <adrien@notk.org>
2014-11-02 15:02:28 +0100

    donations: new page about donating man-time, money or hardware.

---
+++ htdocs/donations.php
@@ -0,0 +1,53 @@
+<?php
+  $page_title = 'Donate';
+  $page_name = 'donate';
+  $page_authors = '<a href="http://win-builds.org">Adrien Nader of win-builds.org</a>';
+
+  include('inc/header.php');
+  include('inc/navigation.php');
+?>
+  <div id="middle">
+    <h2>Donate</h2>
+
+<p>Mingw-w64 is almost entirely made by volunteer. Only two employees are paid 
+to contribute to mingw-w64 and even then it's not full-time: we estimate the 
+paid time spent on the project matches 0.5-employee.
+All other contributions are done on free time.
+</p>
+
+<p>
+There are many evolutions and features planned but resources are scarce. In 
+particular, if mingw-w64 saves you from the steep cost of proprietary 
+toolchains, think about contributing a bit; you will also most likely quickly 
+benefit from the improvements.
+</p>
+
+<h3>Non-financial contributions</h3>
+<p>
+More than through money, the best way to help the project is to invest man-time 
+in it. The list of tasks on the <a href="contribute.php">page about 
+contributing</a> is the best place to start and if you are an employer with 
+trainees, several of these tasks will make good internship.
+</p>
+
+<h3>Donations</h3>
+<p>
+With the current organization and funds of mingw-w64, it wouldn't make sense to 
+try to pay a developer to spend time on specific topics. Instead, the money 
+will be spent on hardware (testing servers, Windows licenses, phone, tablets, 
+...) and meeting expenses (setting up hackatons and paying for travel expenses 
+as some major mingw-w64 developers live very far from others).
+</p>
+
+<p>Currently, only Paypal is available:</p>
+
+<p><a href="https://sourceforge.net/donate/index.php?group_id=202880"><img src="/img/paypal_32.png">Paypal (to Kaï Tietz)</a></p>
+
+<h3>Hardware donations</h3>
+<p>
+There is no facility at the moment to receive hardware. These donations will 
+have to be handled on a case-by-case basis (mostly to decide where to ship).
+</p>
+  </div>
+
+<?php include('inc/footer.php'); ?>
